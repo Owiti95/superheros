93423c5846d8bfd8fe4849842e7604a0d0d55309
hannington owiti <hanningtonowiti@gmail.com>
2024-10-08 20:27:45 +0300

    commenting

--- app.py
+++ app.py
@@ -1,36 +1,19 @@
-from flask import Flask, jsonify, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask import Flask, jsonify, request#importing Flask, jsonify for JSON responses, and request for handling requests
+from flask_sqlalchemy import SQLAlchemy#for database interaction
+from flask_migrate import Migrate#handling database migrations
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///superheroes.db'
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+app = Flask(__name__)#initialising the Flask app
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///superheroes.db'#configuring the database
+db = SQLAlchemy(app)#initialising the SQLAlchemy object with the app
+migrate = Migrate(app, db)# initialising Flask-Migrate for handling database migrations
 
+#basic route that returns a welcome message in JSON format
 @app.route('/')
 def index():
     return jsonify({"message": "Welcome to the Superhero API"})
 
+
+# running the app in debug mode
 if __name__ == '__main__':
     app.run(debug=True)
 
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_migrate import Migrate
-# from main import *
-
-# app = Flask(__name__)
-
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///superheroes.db'
-# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-
-# db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
-
-# @app.route('/')
-# def index():
-#     return jsonify({"message": "superhero API"})
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,11 +1,13 @@
+# Importing the database object from the app module
 from app import db
-
+#represents the 'heroes' table in the database
 class Hero(db.Model):
     __tablename__ = 'heroes'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     super_name = db.Column(db.String(80), nullable=False)
 
+    #relationship with HeroPower table, where a hero can have multiple powers
     hero_powers = db.relationship('HeroPower', backref='hero', cascade='all, delete-orphan')
 
     def to_dict(self):
@@ -22,6 +24,7 @@ class Power(db.Model):
     name = db.Column(db.String(80), nullable=False)
     description = db.Column(db.String(200), nullable=False)
 
+    #relationship with 'HeroPower' table, where a power can be associated with multiple heroes
     hero_powers = db.relationship('HeroPower', backref='power', cascade='all, delete-orphan')
 
     def to_dict(self):
@@ -32,6 +35,7 @@ class Power(db.Model):
         }
 
 
+#defining the HeroPower class, which represents the association table between heroes and powers
 class HeroPower(db.Model):
     __tablename__ = 'hero_powers'
     id = db.Column(db.Integer, primary_key=True)
@@ -39,87 +43,15 @@ class HeroPower(db.Model):
     hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'), nullable=False)
     power_id = db.Column(db.Integer, db.ForeignKey('powers.id'), nullable=False)
 
+#for serializing
     def to_dict(self):
         return {
             "id": self.id,
             "strength": self.strength,
             "hero_id": self.hero_id,
             "power_id": self.power_id,
-            "hero": self.hero.to_dict(),
-            "power": self.power.to_dict(),
+            "hero": self.hero.to_dict(),#converts related hero object to dictionary
+            "power": self.power.to_dict(),#converts related power object to dict
         }
 
 
-
-# # models.py will contain all the database models ( how we interact with the database )
-# # we use Flask-SQLAlchemy to create a python class which represents some kind of entry/row for the database/some kind of a table and we define the different columns and data that the class/objects will be 
-
-# from app import db
-
-# # this class represents the "heroes" table in the database
-# class Hero(db.Model):
-#     __tablename__ = 'heroes'
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(80), nulable=False)
-#     super_name = db.Column(db.String(80), nullable=False)
-
-#     # linking each hero to their power by establishing a relationship with the HeroPower model
-#     # backref allows accessibility to the hero object from the HeroPower table using 'hero' attribute
-#     # the 'cascade' option ensures that deleting a hero will also delete their power
-#     hero_powers = db.relationship('HeroPower', backref='hero', cascade='all, delete-orphan')
-
-    
-# # define a function that can take all the
-# # attributes in the object above and convert
-# # it into a python dictionary which we can
-# # then convert into json, something that we
-# # can pass from our API
-
-# # the way we communicate in API is using
-# # something known as json which looks like
-# # a python dictionary. We will be passing
-# # json back and forth so that API will
-# # return json and we will send json to the
-# # API for creating our different objects
-
-#     def to_json(self):
-#         return {
-#             "id": self.id,
-#             "name": self.name,
-#             "super_name": self.super_name
-#         }
-    
-
-# # this class represents the "powers" table in the database
-# class Power(db.Model):
-#     __tablename__ = 'powers'
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(80), nullable=False)
-#     description = db.Column(db.String(200), nullable=False)
-
-#     hero_powers = db.relationship('HeroPower', backref='power', cascade='all, delete-orphan')
-
-#     def to_json(self):
-#         return {
-#             "id": self.id,
-#             "name": self.name,
-#             "description": self.description
-#         }
-
-    
-# class HeroPower(db.Model):
-#     __tablename__ = 'hero_powers'
-#     id = db.Column(db.Integer, primary_key=True)
-#     strength = db.Column(db.String(80), nullable=False)
-#     hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'), nullable=False)
-#     power_id = db.Column(db.Integer, db.ForeignKey('powers.id'), nullable=False)
-
-#     def to_json(self):
-#         return {
-#             "id": self.id,
-#             "strength": self.strength,
-#             "hero_id": self.hero_id,
-#             "power_id": self.power_id,
-#             "hero": self.hero.to_dict(), # returns the related hero's details as a dictionary
-#             "power": self.power.to_dict(), # returns the related power's details as a dictionary
-#         }
